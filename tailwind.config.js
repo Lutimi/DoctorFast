5126fd9c0af58d4dd0497012f4bdea8104c2081c
Lutimi <misael@wemake.pe>
2021-04-10 12:44:06 -0500

    idk

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        celeste: '#32e0c4',
+        dgreen: '#0d7377',
+        plomo:'#eeeeee',
+        nigga: '#212121'
+
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/aspect-ratio'),],
+}
