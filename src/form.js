4ac7a912d8735b2e170614e705fb46105b04b3ab
Lutimi <misael@wemake.pe>
2021-06-03 20:39:00 -0500

    Update index

--- src/form.js
+++ src/form.js
@@ -13,14 +13,37 @@ const addUser = (ev) => {
         placa:document.getElementById('placa').value
     }
     users.push(user)
-    // document.forms[0].reset(); // to clear the form for the next user
-    document.querySelector('form').reset();
+    console.log(user)
+
+
+    const idk = document.querySelector("#showUser")
+
     
-    //for display 
-    console.warn('added', {users} );
-    let pre = document.querySelector('#msg pre')
-    pre.textContent = '\n' + JSON.stringify(users, '\t',2);
-    //saving to local storage
+
+   
+    users.forEach(element => {
+       
+        idk.innerHTML +=   `  <h1 class=" text-white text-2xl" >${ element.nombre}</h1>   
+        <button type="submit" onclick=deleteUser() class="bg-gray-200">
+        Borrar
+      </button> `
+
+      function deleteUser() {
+        idk.innerHTML = ' '
+    }
+    });
+
+   
+
+// generar un button 
+    // // document.forms[0].reset(); // to clear the form for the next user
+    // document.querySelector('form').reset();
+    
+    // //for display 
+    // console.warn('added', {users} );
+    // let pre = document.querySelector('#msg pre')
+    // pre.textContent = '\n' + JSON.stringify(users, '\t',2);
+    // //saving to local storage
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
