5356e180a470134532c3b360dd5c2015185ab9d1
Lutimi <misael@wemake.pe>
2021-06-11 19:11:26 -0500

    Add ajax

--- src/form.js
+++ src/form.js
@@ -1,119 +1,58 @@
 let registrations = [];
 
+window.onload = Init();
 var form2 = document.getElementById('formulario2')
 
+var initData = {
+    id: 1,
+    nombre2: "Juan ",
+    apellido2: "perez",
+    email2: "coder1@hotmail.com",
+    phone: "972730589",
+    edad: 18,
+    temperatura: "34",
+    placa: "AZXINE3",
+};
+
+window.localStorage.setItem("initData", JSON.stringify(initData));
+
+var retriveInitObj = JSON.parse(window.localStorage.getItem("initData"));
+
+var individualObjs = Object.values(retriveInitObj)
+    // console.log(individualObjs[0])
+
 const addUser = (ev) => {
-        ev.preventDefault();
-        let user2 = {
-            id: Date.now(),
-            nombre2: $('#nombre2').val(),
-            apellido2: $('#apellido2').val(),
-            email2: $('#email2').val(),
-            phone: $('#phone').val(),
-            edad: $('#edad').val(),
-            temperatura: $('#temperatura').val(),
-            placa: $('#placa').val(),
-        }
+    ev.preventDefault();
+    let user2 = {
+        id: Date.now(),
+        nombre2: $('#nombre2').val(),
+        apellido2: $('#apellido2').val(),
+        email2: $('#email2').val(),
+        phone: $('#phone').val(),
+        edad: $('#edad').val(),
+        temperatura: $('#temperatura').val(),
+        placa: $('#placa').val(),
+    }
 
-        window.localStorage.setItem('registration', JSON.stringify(user2));
+    window.localStorage.setItem('registration', JSON.stringify(user2));
 
+    var grabRegistrations = JSON.parse(window.localStorage.getItem("registration"));
+    console.log(grabRegistrations)
+    let html2 = "";
+    html2 += `<li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+                <div class="w-full flex items-center justify-between p-6 space-x-6">
+                  <div class="flex-1 truncate">
+                  <h1 class="text-sm " >ID: ${grabRegistrations['id']} </h1>
+                    <div class="flex items-center space-x-3">
 
-
-        // if ($("#nombre2").val().length < 1) {
-        //     alert("El nombre2 es obligatorio");
-        //     return false;
-        // } else if ($("#apellido2").val().length < 1) {
-        //     alert("El apellido2 es obligatorio");
-        //     return false;
-        // } else if ($("#edad").val().length < 1) {
-        //     alert("La edad es obligatorio");
-        //     return false;
-        // } else if ($("#edad").val().length < 1) {
-        //     alert("La temperatura es obligatorio");
-        //     return false;
-        // } else if ($("#edad").val().length < 1) {
-        //     alert("El Corre es obligatorio");
-        //     return false;
-        // }
-
-        registrations.push(user2)
-        console.log(registrations)
-        console.log("user2 added")
-        console.log(user2)
-
-
-
-        // if (registrations != []) {
-        //     idk.innerHTML = registrations[];
-        // }
-
-        // registrations.forEach(element => {
-
-        //     if (element != null || element != [] || elemet.val() > 1) {
-        //         idk.innerHTML += `    <li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-        //     <div class="w-full flex items-center justify-between p-6 space-x-6">
-        //       <div class="flex-1 truncate">
-        //       <h1 class="text-sm " >ID: ${element.id} </h1>
-        //         <div class="flex items-center space-x-3">
-
-        //           <h3 class="text-gray-900 text-sm font-medium truncate">${element.nombre2} ${element.apellido2}</h3>
-        //           <span
-        //             class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">${element.temperatura} grados</span>
-        //         </div>
-        //         <p class="mt-1 text-gray-500 text-sm truncate">email2: ${element.email2}</p>
-        //         <p class="mt-1 text-gray-500 text-sm truncate">CEL: ${element.phone}</p>
-        //         <p class="mt-1 text-gray-500 text-sm truncate">Edad: ${element.edad}</p>
-        //         <p class="mt-1 text-gray-500 text-sm truncate">Placa vehicular: ${element.placa}</p>
-        //       </div>
-        //       <img class="w-12 h-15 bg-gray-300 rounded-full flex-shrink-0"
-        //         src="/public/img/guy.svg"
-        //         alt="">
-        //     </div>
-        //     <div>
-
-        //     </div>
-        //   </li> `
-
-        //     }
-        // });
-
-        registrations.forEach(user => {
-            console.log(user.id);
-
-
-        })
-
-
-    } // document.forms[0].reset(); // to clear the form for the next user2
-
-// // document.querySelector('form2').reset();
-// form2.reset()
-
-// $.getJSON("./Data.json", function(data))
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('enviar').addEventListener('click', addUser);
-});
-
-$.getJSON("../Data.json", function(data) {
-    var idk = document.getElementById("showUser");
-
-    console.log(data);
-    data.forEach(d => {
-        var data_list = `<li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-            <div class="w-full flex items-center justify-between p-6 space-x-6">
-              <div class="flex-1 truncate">
-              <h1 class="text-sm " >ID: ${d.id} </h1>
-                <div class="flex items-center space-x-3">
-
-                  <h3 class="text-gray-900 text-sm font-medium truncate">${d.nombre2} ${d.apellido2}</h3>
+                  <h3 class="text-gray-900 text-sm font-medium truncate">${grabRegistrations['nombre2']} ${grabRegistrations['apellido2']}</h3>
                   <span
-                    class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">${d.temperatura} grados</span>
+                    class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">${grabRegistrations['temperatura']} grados</span>
                 </div>
-                <p class="mt-1 text-gray-500 text-sm truncate">email2: ${d.email2}</p>
-                <p class="mt-1 text-gray-500 text-sm truncate">CEL: ${d.phone}</p>
-                <p class="mt-1 text-gray-500 text-sm truncate">Edad: ${d.edad}</p>
-                <p class="mt-1 text-gray-500 text-sm truncate">Placa vehicular: ${d.placa}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">email: ${grabRegistrations['email2']}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">CEL: ${grabRegistrations['phone']}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">Edad: ${grabRegistrations['age']}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">Placa vehicular: ${grabRegistrations['placa']}</p>
               </div>
               <img class="w-12 h-15 bg-gray-300 rounded-full flex-shrink-0"
                 src="/public/img/guy.svg"
@@ -122,7 +61,38 @@ $.getJSON("../Data.json", function(data) {
             <div>
 
             </div>
-          </li> `
-        idk.innerHTML = data_list;
-    })
+          </li> `;
+    $("#showUser").append(html2);
+}
+
+
+
+let html = "";
+html += `<li class="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+            <div class="w-full flex items-center justify-between p-6 space-x-6">
+              <div class="flex-1 truncate">
+              <h1 class="text-sm " >ID: ${individualObjs[0]} </h1>
+                <div class="flex items-center space-x-3">
+
+                  <h3 class="text-gray-900 text-sm font-medium truncate">${individualObjs[1]} ${individualObjs[2]}</h3>
+                  <span
+                    class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">${individualObjs[6]} grados</span>
+                </div>
+                <p class="mt-1 text-gray-500 text-sm truncate">email2: ${individualObjs[4]}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">CEL: ${individualObjs[5]}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">Edad: ${individualObjs[3]}</p>
+                <p class="mt-1 text-gray-500 text-sm truncate">Placa vehicular: ${individualObjs[7]}</p>
+              </div>
+              <img class="w-12 h-15 bg-gray-300 rounded-full flex-shrink-0"
+                src="/public/img/guy.svg"
+                alt="">
+            </div>
+            <div>
+
+            </div>
+          </li> `;
+$("#showUser").append(html);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("enviar").addEventListener("click", addUser);
 });
--- src/main.js
+++ src/main.js
@@ -2,57 +2,74 @@ CerrarSesion = document.querySelector(".modalCerrarSesion")
 avisoRegistro = document.querySelector(".modalAvisoTxt");
 
 
-//Mostrar array de usurios
-// JSON.parse(
-//     localStorage.getItem("users")
-//     )
+
+const LocalStorageGlobalVariables = {
+    All_Registrations: "All_Registrations",
+};
+
+
+const GetLocalStorage = () => localStorage;
+const GetLocalStorageItem = (keyName) => localStorage.getItem(keyName);
+const SetLocalStorage = (keyName, value) =>
+    localStorage.setItem(keyName, value);
 
 
 
+// Convert
+const ConvertToJSON = (text) => JSON.parse(text);
+const ConvertToString = (obj) => JSON.stringify(obj);
 
-// var wm_defaults = {
-//     viewOffset:
-//     {
-//         top: 0,
-//         right: 0,
-//         bottom: 000,
-//         left: 0
-//     },
-//     scale: 0.8,
-//     reset: false,
-//     duration: '1000'
-// };
-// var dist = '100px';
 
-// ScrollReveal()
-//     .reveal('.reveal', wm_defaults);
-// ScrollReveal()
-//     .reveal('.delay-100', { delay: 100 });
-// ScrollReveal()
-//     .reveal('.delay-200', { delay: 200 });
-// ScrollReveal()
-//     .reveal('.delay-300', { delay: 300 });
-// ScrollReveal()
-//     .reveal('.delay-400', { delay: 400 });
-// ScrollReveal()
-//     .reveal('.delay-800', { delay: 800 });
-// ScrollReveal()
-//     .reveal('.r-top', {
-//         origin: 'top',
-//         distance: dist
-//     });
-// ScrollReveal()
-//     .reveal('.r-bottom', {
-//         origin: 'bottom',
-//         distance: dist
-//     });
-// ScrollReveal()
-//     .reveal('.r-left', {
-//         origin: 'left',
-//         distance: dist
-//     });
-// ScrollReveal()
-//     .reveal('.r-right', {
-//         origin: 'right',
-//         distance: dist
-//     });
+
+//load data base with json
+const Init = () => {
+    LoadDataBaseFromJSON();
+    // RenderregistersList();
+};
+
+const LoadDataBaseFromJSON = () => {
+    $.getJSON("../Data.json", function(registers) {
+        let registersJSON = ConvertToString(registers);
+        SetLocalStorage(LocalStorageGlobalVariables.All_Registrations, registersJSON);
+    });
+};
+
+
+
+const RenderRegistrationList = () => {
+    //Primero limpiamos todo la seccion del render
+    // $("#showUser").html("");
+
+    //Obtengo los productos de mi Dase de datos
+    let RegistersText = GetLocalStorageItem(
+        LocalStorageGlobalVariables.All_Registrations
+    );
+
+    //los convierto a objeto literal
+    let registersJSON = ConvertToJSON(RegistersText);
+
+    //recorremos el JSON y armamos nuestro HTML
+    let registerToRender = [];
+    $.each(registersJSON, function(key, product) {
+        registerToRender.push(ProductCard(product));
+    });
+
+    //renderizo en mi web
+    $("#showUser").html(registerToRender);
+};
+
+const ProductCard = (product) => `<div class="card" id=${
+    "card-product-" + product.id
+  } style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">${product.nombre2}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Stock: ${
+              product.apellido2
+            }</h6>
+         
+            <a onclick="AddProduct(${
+              product.id
+            })" class="btn btn-primary">Agregar al carrito</a>
+            
+          </div>
+        </div>`;
